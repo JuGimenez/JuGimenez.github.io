0b29725a7ed443179b6563139d435d4de9e444aa
Tadasheiros <tadashi.nbd@gmail.com>
2023-01-06 18:59:17 -0300

    Começando a tomar forma

--- ecobike-app/src/App.js
+++ ecobike-app/src/App.js
@@ -1,9 +1,15 @@
 import Navbar1 from './Navbar1';
+import Gallery from './Gallery';
+import Texts from './Texts';
+import Footer from './Footer';
 
 function App() {
   return (
     <div>
       <Navbar1 />
+      <Gallery /> 
+      <Texts />
+      <Footer />
     </div>
   );
 }
--- ecobike-app/src/Navbar1.js
+++ ecobike-app/src/Navbar1.js
@@ -1,29 +1,21 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function Navbar1() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="light" expand="lg" sticky='top'>
       <Container>
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#home">Sobre a Pesquisa</Nav.Link>
+            <Nav.Link href="#link">Produto Educacional</Nav.Link>
+            <Nav.Link href="#ancora1">Sequência Didática</Nav.Link>
+            <Nav.Link href="#ancora2">Resultados e Discussão</Nav.Link>
+            <Nav.Link href="#ancora3">Galeria</Nav.Link>
+            <Nav.Link href="#ancora4">Contato</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
